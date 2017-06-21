8adbf5a9a21850c28a7364b16fb0e8d891691250
Ritsuki Goto <kawakawaritsuki.dev@gmail.com>
2017-06-21 11:41:55 +0900

    [add] First commit

---
+++ Assets/Scripts/FileLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FileLoader{
+
+    string data = ":SONGNAME=ウミユリ海底譚\n:ARTIST=n-buna\n:BPM=120\n\n:START\n----L==LY==Y----\n----L==L--------\n----L==L--------\n----L==L--------\n----L==LR==R----\n----L==LR==R----\n----L==LR==R----\nR==R--------R==R\nR==R--------R==R\n----R==R--------\nR==R--------R==R\n--------R==R----\nR==R--------R==R\n----R==R--------\nR==R--------R==R\n--------R==R----\nR==R--------R==R\n----R==R--------\nR==R--------R==R\n--------R==R----\nR==R--------R==R\n----R==R--------\nY==Y--------Y==Y\n-\nR==R--------R==R\n--------R==R----\nR==R--------R==R\n----R==R--------\nR==R--------R==R\n--------R==R----\nR==R--------R==R\n----R==R------\n--\nR==R--------R==R\n--------R==R----\nR==R--------R==R\n----R==R--------\nR==R--------R==R\n--------R==R----\nY==Y--------Y==Y\n:END";
+    int start = 0;
+
+    public FileLoader(string text)
+    {
+        // data = text;
+        string[] split = data.Split('\n');
+        // この辺にちゃんとしたチェック挟む
+
+        for (int i = 0; i < split.Length; ++i)
+        {
+            if(split[i] == ":START")
+            {
+                start = i + 1;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+    public GameObject prefab;
+    public float interval = 1;
+    float timer = 0;
+    int i = 0;
+
+    void Update()
+    {
+        timer -= Time.deltaTime; //タイマーを減らす
+        if (timer < 0)
+        { //タイマーがゼロより小さくなったら
+            Spawn(); // Spawnメソッドを呼ぶ
+            timer = interval; // タイマーをリセットする
+        }
+    }
+    // 敵を生成するメソッド
+    void Spawn()
+    {
+        GameObject red = Instantiate(prefab);
+        Red r = red.GetComponent<Red>();
+        r.Create(4,4);
+        i++;
+        if(i == 15) { i = 0; }
+    }
+}
