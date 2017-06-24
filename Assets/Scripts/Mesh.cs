4da124e0ec10dd9a9a8d09aea062e19ac3d33a9d
Ritsuki Goto <kawakawaritsuki.dev@gmail.com>
2017-06-24 10:23:15 +0900

    [add] Slide Object

---
+++ Assets/Scripts/Mesh.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(MeshRenderer))]
+[RequireComponent (typeof(MeshFilter))]
+public class Mesh : MonoBehaviour
+{
+	private void Start ()
+	{
+		UnityEngine.Mesh mesh = new UnityEngine.Mesh ();
+		mesh.vertices = new Vector3[] {
+//			new Vector3 (0, 0, 0),
+//			new Vector3 (4, 0, 0),
+//			new Vector3 (0, 0, 0.7f),
+//			new Vector3 (4, 0, 0.7f),
+			new Vector3 (0, 0, 0),
+			new Vector3 (4, 0, 0),
+			new Vector3 (-3, 0, 15f),
+			new Vector3 (1, 0, 15f),
+		};
+		mesh.triangles = new int[] {
+			1, 2, 3,0, 2, 1
+		};
+
+		var filter = GetComponent<MeshFilter> ();
+		filter.sharedMesh = mesh;
+	}
+	void FixedUpdate()
+	{
+		transform.position += new Vector3(0, 0, 120 / -3 / 5 * 1.5f * Time.deltaTime * 3);
+	}
+}
